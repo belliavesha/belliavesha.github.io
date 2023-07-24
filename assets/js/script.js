2460ef1ae07169c1662bba47de4028dad6874892
belliavesha <belliavesha@gmail.com>
2023-07-24 08:39:27 -0400

    add presentations and backgrounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,3 +63,60 @@ function updateIndex(delta) {
     var flipbook = document.getElementById('profile-pic');
     flipbook.src = 'assets/images/alive_2/frame' + String(index).padStart(2, '0') + '.jpg';
 }
+
+
+// // Get all buttons that open modals
+// var btns = document.getElementsByClassName("openModal");
+
+// // Loop through the buttons and assign a click event handler to each
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].onclick = function() {
+//         // Get the modal id from the data-modal attribute of the button
+//         var modalId = this.getAttribute("data-modal");
+
+//         // Get the modal and the close button
+//         var modal = document.getElementById(modalId);
+//         var span = modal.getElementsByClassName("close")[0];
+
+//         // Show the modal
+//         modal.style.display = "block";
+
+//         // When the user clicks on <span> (x), close the modal
+//         span.onclick = function() {
+//             modal.style.display = "none";
+//         }
+
+//         // When the user clicks anywhere outside of the modal, close it
+//         window.onclick = function(event) {
+//             if (event.target == modal) {
+//                 modal.style.display = "none";
+//             }
+//         }
+//     }
+// }
+
+
+function loadVideo(filename) {
+    // Get the button
+    var button = document.getElementById('videoButton');
+
+    // Create a new video element
+    var video = document.createElement('video');
+
+    // Set the source of the video
+    video.src = filename;
+
+    // Add controls to the video
+    video.controls = true;
+
+    // Add an event listener to the video that will change it back to a button when it ends
+    video.addEventListener('ended', function() {
+        button.innerHTML = 'Load Video 1';
+        button.onclick = function() { loadVideo('movie1.mp4'); };
+    });
+
+    // Change the button to the video
+    button.innerHTML = '';
+    button.appendChild(video);
+    button.onclick = null;
+}
